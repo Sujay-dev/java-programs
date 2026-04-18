2692f23afc056c1309ac38b93ae24a3ebf7a0a22
Sujay Dev <sujaykamble006@gmail.com>
2026-04-18 13:40:40 +0530

    Update Array2.java

--- Methods/Method With Array/Array2.java
+++ Methods/Method With Array/Array2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Array2 {
     public static int[] right(int arr[]){
         if (arr.length==0) return arr;
-        System.out.println("After Rotating by 1 : ");
+        System.out.println("After Rotating Right by 1 : ");
 
         int[]res=arr.clone();
         int temp=res[res.length-1];
@@ -18,31 +18,57 @@ public class Array2 {
         return res;
     }
 
-    public static void zeros(int arr[]){
+    public static void frequency(int arr[]){
+        System.out.println("Frequency of element in array is : ");
+        for(int i=0; i<arr.length; i++){
+            boolean found=false;
+            for(int j=0; j<i; j++){
+                if (arr[i]==arr[j]) {
+                    found=true;
+                    break;
+                }
+            }
+            if (found) {
+                continue;
+            }
+
+            int count=1;
+
+            for(int k=i+1; k<arr.length; k++){
+                if (arr[k]==arr[i]) {
+                    count++;
+                }
+            }
+
+            System.out.println(arr[i]+" : "+ count);
+        }
+    }
+
+    public static int[] zeros(int arr[]){
         System.out.println("After moving zeros to end : ");
         int index=0;
+        int res[]=arr.clone();
 
-        for(int i=0; i<arr.length; i++){
-            if (arr[i]!=0) {
-                arr[index]=arr[i];
+        for(int i=0; i<res.length; i++){
+            if (res[i]!=0) {
+                res[index]=res[i];
                 index++;
             }
         }
-        while (index<arr.length) {
-            arr[index]=0;
+        while (index<res.length) {
+            res[index]=0;
             index++;
         }
-
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i]+" ");
-        }
+        return res;
     }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
+        System.out.println("Enter Size of Array : ");
         int size=sc.nextInt();
         int arr[]=new int[size];
+        System.out.println("Enter Array Elements : ");
         for(int i=0; i<size; i++){
             arr[i]=sc.nextInt();
         }
@@ -53,7 +79,9 @@ public class Array2 {
         }
          System.out.println();
         System.out.println(Arrays.toString(right(arr)));
-        zeros(arr);
+        frequency(arr);
+        System.out.println(Arrays.toString(zeros(arr)));
         sc.close();
     }
 }
+
