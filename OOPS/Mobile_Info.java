7161ca61cbee4143da0aa5186d1109f2c047e1e4
Sujay Dev <sujaykamble006@gmail.com>
2026-05-01 16:34:05 +0530

    Update Mobile_Info.java

--- OOPS/Mobile_Info.java
+++ OOPS/Mobile_Info.java
@@ -3,21 +3,30 @@ package OOPS;
 import java.util.Scanner;
 
 class Mobile {
-    String brand;
-    int battery;
+    private String brand;
+    private int battery;
+
+    Mobile(String brand,int battery){
+        this.brand=brand;
+        this.battery=battery;
+    }
     
-    void charge(int percent){
+    public void charge(int percent){
         battery+=percent;
-        if (battery>100) {
+        if (battery>=100) {
             battery=100;
         }
         }
 
-    void use(int percent){
-        battery-=percent;
-        if (battery<0) {
+   public void use(int percent){
+    battery-=percent;
+        if (battery<=0) {
             battery=0;
         }
+    }
+
+    public int getbattery(){
+        return battery;
     }
 
     void display(){
@@ -29,14 +38,13 @@ public class Mobile_Info {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
-        Mobile m1=new Mobile();
-        System.out.println("Enter brand of mobile : ");
-        m1.brand=sc.nextLine();
-        System.out.println("Enter battery of mobile : ");
-        m1.battery=sc.nextInt();
+        String brand=sc.nextLine();
+        int battery=sc.nextInt();
 
+        Mobile m1=new Mobile(brand,battery);
         m1.charge(50);
         m1.use(20);
+        System.out.println(m1.getbattery());
         m1.display();
 
         sc.close();
