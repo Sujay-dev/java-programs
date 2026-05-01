9881a441b9737fcfd2ba56f5e13c4d01d21c0e93
Sujay Dev <sujaykamble006@gmail.com>
2026-05-01 16:37:12 +0530

    Update Bank_Info.java

--- OOPS/Bank_Info.java
+++ OOPS/Bank_Info.java
@@ -2,35 +2,63 @@ package OOPS;
 
 import java.util.Scanner;
 
-class Account{
-     String name;
-     int balance;
+class Bank {
+     private String name;
+     private int balance;
 
-     void deposit(int amount){
+     Bank(String name, int balance){
+        this.name=name;
+        this.balance=balance;
+     }
+
+    public void deposit(int amount){
+        if (amount<=0) {
+            System.out.println("Invalid amount");
+        }else{
         balance+=amount;
-     }
-     void withdrawl(int amount){
-        if (amount<=balance) {
-            balance-=amount;
+    }
+}
+    public void withdraw(int amount){
+        if(amount<=0){
+            System.out.println("Invalid amount");
+        }else if (amount<=balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient balance");
         }
-        else{
-            System.out.println("Insufficient Balance");
-        }
-     }
-     void displaybalance(){
-        System.out.println("Balance: "+ balance);
-     }
+    }
+    public int getbalance(){
+        return balance;
+    }
+    public void diplay(){
+        System.out.println("Name : "+ name);
+        System.out.println("Balance : "+ balance);
+    }
 }
 public class Bank_Info {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        Account a1=new Account();
-        a1.name=sc.next();
-        a1.balance=sc.nextInt();
-        a1.deposit(5000);
-        a1.withdrawl(3000);
+      
+        System.out.println("Enter name : ");
+        String name=sc.nextLine();
+        System.out.println("Enter Balance : ");
+        int balance=sc.nextInt();
 
-        a1.displaybalance();
+        Bank b1=new Bank(name,balance);
+        System.out.println("Enter Deposited amount : ");
+        int depo=sc.nextInt();
+        b1.deposit(depo);
+        System.out.println("Balance after depositing amount : ");
+        System.out.println(b1.getbalance());
+        sc.nextLine();
+        System.out.println("Enter Withdraw amount : ");
+        int withdra=sc.nextInt();
+        b1.withdraw(withdra);
+        System.out.println("Balance after withdrawl : ");
+        System.out.println(b1.getbalance());
+        System.out.println("Final balance : ");
+        b1.diplay();
         sc.close();
+
     }
 }
