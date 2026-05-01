b570d7f136ae6caf793192afaf3059e7e8d8e60f
Sujay Dev <sujaykamble006@gmail.com>
2026-05-01 16:34:50 +0530

    Update Student_Grade.java

--- OOPS/Student_Grade.java
+++ OOPS/Student_Grade.java
@@ -3,25 +3,39 @@ package OOPS;
 import java.util.Scanner;
 
 class Students {
-    String name;
-    int marks;
-    
-    char grade(){
-        if (marks > 100 || marks < 0){
-            return 'X';
-        }
-        if (marks>=90) {
-            return 'A';
-        }else if (marks>=75) {
-            return 'B';
-        }else if (marks>=50) {
-            return 'C';
+   private String name;
+    private int marks;
+
+    Students(String name, int marks){
+        this.name=name;
+        setmarks(marks);
+    }
+
+    public void setmarks(int marks){
+        if (marks>=0 && marks<100) {
+            this.marks=marks;
         }else{
-            return 'F';
+            this.marks=0;
         }
     }
 
-    void display(){
+    public int getmarks(){
+        return marks;
+    }
+    
+    String grade(){
+        if (marks>=90) {
+            return "A";
+        }else if (marks>=75) {
+            return "B";
+        }else if (marks>=50) {
+            return "C";
+        }else{
+            return "Fail";
+        }
+    }
+
+   public void display(){
         System.out.println("Name : "+name+" ,"+ "Marks : "+marks+ " ," +"Grade : "+ grade());
     }
 }
@@ -30,11 +44,13 @@ public class Student_Grade {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
-        Students s1=new Students();
-        System.out.println("Enter name of student : ");
-        s1.name=sc.nextLine();
-        System.out.println("Enter marks of student : ");
-        s1.marks=sc.nextInt();
+        System.out.println("Enter your name : ");
+        String name=sc.nextLine();
+        System.out.println("Enter your marks : ");
+        int marks=sc.nextInt();
+
+        Students s1=new Students(name,marks);
+        System.out.println("Marks : "+ s1.getmarks());
 
         s1.display();
         sc.close();
