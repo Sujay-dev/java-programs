ebdea9e8372a98606a5ff43623fbe049c120dcf0
Sujay Dev <267361482+Sujay-dev@users.noreply.github.com>
2026-05-23 15:54:35 +0530

    Add files via upload

---
+++ OOPS/Interface/Payment_Gateway.java
@@ -0,0 +1,39 @@
+package OOPS.Interface;
+
+import java.util.Scanner;
+
+interface Payment {
+    public int pay(int amount);    
+}
+
+interface Security {
+    public boolean authenticate(String password);    
+}
+
+class SecurePayment implements Payment,Security {
+    public boolean authenticate(String password){
+        return password.equals("1234");
+    }
+        public int pay(int amount){
+            return (amount+(amount*3)/100);
+        }
+
+}
+public class Payment_Gateway {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        String password=sc.nextLine();
+        int amount=sc.nextInt();
+
+        SecurePayment s=new SecurePayment();
+
+         if(s.authenticate(password)){
+            System.out.println("Payment Success ");
+          System.out.println("Paid: " + s.pay(amount));
+          }else{
+          System.out.println("Payment failed Wrong password ");
+          }
+        sc.close();
+    }
+}
