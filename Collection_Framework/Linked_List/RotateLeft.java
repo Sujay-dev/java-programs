444b8f781b4a80e96d6f8ca38c8b5b18fc2c769b
Sujay Dev <267361482+Sujay-dev@users.noreply.github.com>
2026-06-09 13:23:54 +0530

    Add files via upload

---
+++ Collection_Framework/Linked_List/RotateLeft.java
@@ -0,0 +1,23 @@
+package CollectionFramework.LinkedList;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class RotateLeft {
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+       LinkedList<Integer> list=new LinkedList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+
+        int first = list.removeFirst();
+        list.addLast(first);
+
+        System.out.println(list);
+        sc.close();
+    }
+}
