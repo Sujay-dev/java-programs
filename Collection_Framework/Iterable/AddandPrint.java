02121b2e2c9f77b757711235e3aa265e62b3f46c
Sujay Dev <267361482+Sujay-dev@users.noreply.github.com>
2026-06-26 19:48:15 +0530

    Add AddandPrint class to print ArrayList elements

---
+++ Collection_Framework/Iterable/AddandPrint.java
@@ -0,0 +1,20 @@
+package CollectionFramework.Iterable;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class AddandPrint {
+        public static void main(String[] args) {
+        ArrayList<String> list=new ArrayList<>();
+
+        list.add("Java");
+        list.add("Spring");
+        list.add("Sql");
+
+        Iterator<String> it=list.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
