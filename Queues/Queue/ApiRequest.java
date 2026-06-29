5a82be2122427443870bbed9c6b85efce06aa671
Sujay Dev <267361482+Sujay-dev@users.noreply.github.com>
2026-06-30 00:55:06 +0530

    Refactor ApiRequest to handle Api objects

--- Queues/Queue/ApiRequest.java
+++ Queues/Queue/ApiRequest.java
@@ -3,20 +3,42 @@ package Queues.Queue;
 import java.util.LinkedList;
 import java.util.Queue;
 
+class Api{
+    private int requestId;
+    private String endpoint;
+
+    Api(int requestId,String endPoint){
+        this.requestId=requestId;
+        this.endpoint=endPoint;
+    }
+    public int getId(){
+        return requestId;
+    }
+    public String getEndPoint(){
+        return endpoint;
+    }
+}
 public class ApiRequest {
     public static void main(String[] args) {
-        Queue<Integer> queue=new LinkedList<>();
+        Queue<Api> queue=new LinkedList<>();
 
-        queue.offer(101);
-        queue.offer(102);
-        queue.offer(103);
-        queue.offer(104);
-        queue.offer(105);
+        Api a1=new Api(101, "/login");
+        Api a2=new Api(102, "/users");
+        Api a3=new Api(103, "/orders");
+        Api a4=new Api(104, "/paments");
+        Api a5=new Api(105, "/logout");
+
+        queue.offer(a1);
+        queue.offer(a2);
+        queue.offer(a3);
+        queue.offer(a4);
+        queue.offer(a5);
 
         while (!queue.isEmpty()) {
-           Integer request=queue.poll();
-           System.out.println("Processing: " + request);
+            Api request=queue.poll();
+            System.out.println("Processing Request: " + request.getId() + " -> " + request.getEndPoint());
         }
     }
 
 }
+
