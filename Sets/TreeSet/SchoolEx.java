8b97819b5711bc01283bae3a746460547e7508e1
Sujay Dev <267361482+Sujay-dev@users.noreply.github.com>
2026-07-06 17:48:16 +0530

    Add files via upload

--- Sets/TreeSet/SchoolEx.java
+++ Sets/TreeSet/SchoolEx.java
@@ -1,4 +1,4 @@
-package Revision;
+package Revision_2;
 
 import java.util.Comparator;
 import java.util.TreeSet;
@@ -8,12 +8,18 @@ class School{
     private String name;
     private int marks;
 
-    School(int id,String name,int marks){
+    School(int id,String name, int marks){
         this.id=id;
         this.name=name;
         this.marks=marks;
     }
-    public int getID(){
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setMarks(int marks){
+        this.marks=marks;
+    }
+    public int getId(){
         return id;
     }
     public String getName(){
@@ -25,16 +31,32 @@ class School{
 }
 public class SchoolEx {
     public static void main(String[] args) {
-        TreeSet<School> set=new TreeSet<>(Comparator.comparing(School :: getMarks).reversed().thenComparing(Comparator.comparing(School :: getName).thenComparing(School :: getID)));
+        TreeSet<School> set=new TreeSet<>(Comparator.comparingInt(School :: getMarks).reversed().thenComparing(Comparator.comparing(School :: getName).thenComparing(School :: getId)));
 
-        set.add(new School(101, "Sujay", 82));
-        set.add(new School(102, "Suyash", 68));
-        set.add(new School(101, "Vedant", 63));
-        set.add(new School(104, "Suyash", 80));
-        set.add(new School(105, "Gaurav", 78));
+        School s1=new School(101, "Sujay", 82);
+        School s2=new School(102, "Suyash", 68);
+        School s3=new School(103, "Vedant", 63);
+        School s4=new School(104, "Rohan", 68);
+        School s5=new School(105, "Athrava", 70);
+        School s6=new School(106, "Abhi", 70);
 
+        set.add(s1);
+        set.add(s2);
+        set.add(s3);
+        set.add(s4);
+        set.add(s5);
+        set.add(s6);
+
+        System.out.println();
         for (School s : set) {
-            System.out.println(s.getID() + " " + s.getName() + " " +s.getMarks());
+            System.out.println(s.getId() + " " + s.getName() + " " +s.getMarks());
         }
+
+        School first=set.first();
+        set.remove(first);
+        School second=set.first();
+        System.out.println("\nTop Two Students: ");
+        System.out.println(first.getId() + " " +first.getName() + " " + first.getMarks());
+        System.out.println(second.getId() + " " + second.getName() + " " + second.getMarks());
     }
 }
